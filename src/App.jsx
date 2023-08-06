afcdc51ec94e3d356703849dbc40c99d79316c43
rohan3011 <rohankamble3011@gmail.com>
2023-08-06 12:02:17 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import { Button } from "./components/Button";
+
+function App() {
+  return (
+    <div className="bg-red-500">
+      <h1>Reusable Components with jsdoc</h1>
+      <Button className="bg-emerald-500">Hello World</Button>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Button.jsx
@@ -0,0 +1,39 @@
+import { cva } from "class-variance-authority";
+import { cn } from "../lib/utils";
+
+const buttonVariants = cva(
+  "inline-flex items-center justify-center rounded shadow",
+  {
+    variants: {
+      variant: {
+        default: "bg-blue-500 text-white hover:bg-blue-400",
+        outline: "bg-transparent border border-slate-200",
+      },
+      size: {
+        default: "h-10 px-3 py-2",
+      },
+    },
+    defaultVariants: {
+      variant: "default",
+      size: "default",
+    },
+  }
+);
+
+/**
+ * @typedef {React.HTMLAttributes<HTMLButtonElement>
+ *  & import("class-variance-authority").VariantProps<typeof buttonVariants>} ButtonProps
+ *
+ *
+ * @param {ButtonProps} props
+ */
+function Button({ className, variant, size, ...props }) {
+  return (
+    <button
+      className={cn(buttonVariants({ variant, size, className }))}
+      {...props}
+    />
+  );
+}
+
+export { Button, buttonVariants };
